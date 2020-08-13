67cb475e0bbbde85c60e7b274dbdbafb12a734b6
zhaodan <zdailwh@sina.com>
2020-08-13 15:30:50 +0800

    我的钱包及提现

--- pages/my/wallet/index.js
+++ pages/my/wallet/index.js
@@ -1,66 +1,140 @@
-// pages/my/wallet/index.js
+var hasClick = false
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    account: {},
+    showSubmenu: false,
+    currMenu: '',
+    records: [],
+    pageSize: 5,
+    getRecordsFlag: true,
+    currPage: 0,
+    noMoreRecordsFlag: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getAccount()
+    this.getRecords()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  toggleShowSubmenu () {
+    this.setData({
+      showSubmenu: !this.data.showSubmenu
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  toggleSubmenu(e) {
+    this.setData({
+      currMenu: e.target.dataset.menu,
+      showSubmenu: false,
+      getRecordsFlag: true,
+      currPage: 0,
+      noMoreRecordsFlag: false
+    })
+    this.getRecords()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  getAccount () {
+    var self = this
 
+    if (hasClick) return
+    hasClick = true
+    wx.showLoading()
+
+    wx.request({
+      url: app.globalData.baseUrl + '/transaction/v1/account',
+      method: 'GET',
+      header: { 'Authorization': 'Bearer ' + wx.getStorageSync('ashibro_Authorization') },
+      success: function (res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          self.setData({
+            account: res.data
+          })
+        } else {
+          if (res.data.msg && res.data.msg.indexOf('Token Expired') !== -1) {
+            wx.navigateTo({
+              url: '/pages/user/auth/auth',
+            })
+          } else {
+            wx.showToast({ title: res.data.msg, icon: 'none' })
+          }
+        }
+      },
+      fail: function (res) {
+        wx.showToast({ title: '系统错误', icon: 'none' })
+      },
+      complete: function (res) {
+        hasClick = false
+        wx.hideLoading()
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  getRecords() {
+    var self = this
+    var params = {}
+    if (this.data.currMenu) {
+      params.type = this.data.currMenu
+    }
 
+    if (!this.data.getRecordsFlag) return
+    this.setData({ getRecordsFlag: false })
+    wx.showLoading()
+
+    wx.request({
+      url: app.globalData.baseUrl + '/transaction/v1/records',
+      method: 'GET',
+      header: { 'page': this.data.currPage, 'page-size': this.data.pageSize, 'Authorization': 'Bearer ' + wx.getStorageSync('ashibro_Authorization') },
+      data: params,
+      success: function (res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          if (res.data.length < self.data.pageSize) {
+            var reqState = false
+            self.setData({
+              noMoreRecordsFlag: true
+            })
+          } else {
+            var reqState = true
+          }
+          res.data.forEach((item, index, array) => {
+            item.created = item.created ? app.globalData.moment.utc(item.created).format('YYYY-MM-DD hh:mm:ss') : ''
+            var idxOfFlag = item.content.indexOf('+') || item.content.indexOf('-') || 0
+            item.value = item.content.substring(idxOfFlag)
+          })
+
+          var list = self.data.records.concat(res.data)
+          var nextPage = ++self.data.currPage
+          self.setData({
+            records: list,
+            getRecordsFlag: reqState,
+            currPage: nextPage
+          })
+        } else {
+          self.setData({
+            getRecordsFlag: true
+          })
+          if (res.data.msg && res.data.msg.indexOf('Token Expired') !== -1) {
+            wx.navigateTo({
+              url: '/pages/user/auth/auth',
+            })
+          } else {
+            wx.showToast({ title: res.data.msg, icon: 'none' })
+          }
+        }
+      },
+      fail: function (res) {
+        wx.showToast({ title: '系统错误', icon: 'none' })
+        self.setData({
+          getRecordsFlag: true
+        })
+      },
+      complete: function (res) {
+        wx.hideLoading()
+      }
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onReachBottom() {
+    this.getRecords()
   }
 })
--- pages/my/wallet/pickup/pickup.js
+++ pages/my/wallet/pickup/pickup.js
@@ -1,17 +1,90 @@
-// pages/my/wallet/pickup/pickup.js
+var app = getApp()
+var hasClick = false
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    total: 679,
-    pickupInput: ''
+    balance: null,
+    formdata: {
+      total_fee: null
+    }
   },
-  getall() {
-    this.setData({ pickupInput: this.data.total })
+
+  onLoad(opt) {
+    this.setData({
+      balance: opt.balance
+    })
   },
+
   radioChange(e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+
+  //input表单数据绑定
+  inputInfo: function (e) {
+    let dataset = e.currentTarget.dataset;
+    let value = e.detail.value;
+    this.data[dataset.obj][dataset.item] = value;
+    this.setData({
+      formdata: this.data[dataset.obj]
+    })
+  },
+
+  getall() {
+    var form = this.data.formdata
+    form.total_fee = this.data.balance
+    this.setData({ formdata: form })
+  },
+
+  // 提现
+  toWithdrawals() {
+    var self = this
+
+    if (!parseFloat(this.data.formdata.total_fee)) {
+      wx.showToast({ title: '请填写提现金额', icon: 'none' })
+      return
+    } else if (parseFloat(this.data.formdata.total_fee) > parseFloat(this.data.balance)) {
+      wx.showToast({ title: '余额不足', icon: 'none' })
+      return
+    }
+
+    if (hasClick) return
+    hasClick = true
+    wx.showLoading()
+
+    wx.request({
+      url: app.globalData.baseUrl + '/transaction/v1/withdrawals',
+      method: 'POST',
+      header: { 'Authorization': 'Bearer ' + wx.getStorageSync('ashibro_Authorization') },
+      data: { total_fee: parseFloat(this.data.formdata.total_fee), method: 'miniprogram' }, 
+      success: function (res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          if (res.data.status === 'success') {
+            wx.showToast({ title: '提现成功', icon: 'success' })
+            wx.navigateBack({
+              delta: 1
+            })
+          }
+        } else {
+          console.log(res)
+          if (res.data.msg && res.data.msg.indexOf('Token Expired') !== -1) {
+            wx.navigateTo({
+              url: '/pages/user/auth/auth',
+            })
+          } else {
+            wx.showToast({ title: res.data.msg, icon: 'none' })
+          }
+        }
+      },
+      fail: function (res) {
+        wx.showToast({ title: '系统错误', icon: 'none' })
+      },
+      complete: function (res) {
+        wx.hideLoading()
+        hasClick = false
+      }
+    })
   }
 })
