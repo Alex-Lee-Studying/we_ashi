b14213f150e906a784f562396cd1d12ce3924ce3
zhaodan <zdailwh@sina.com>
2020-10-09 16:54:11 +0800

    修改会话页面针对系统通知和官方客服的头像；求带支付成功后发送卡片给出行人

--- pages/delivery/result/result.js
+++ pages/delivery/result/result.js
@@ -4,13 +4,15 @@ Page({
   data: {
     deliveryId: null,
     delivery: {},
-    payment: {}
+    payment: {},
+    target_user_id: ''
   },
 
   onLoad(option) {
     console.log(option)
     if (option.id) {
       this.setData({ deliveryId: option.id })
+      this.getDelivery()
       this.getPayResult()
     } else {
       wx.redirectTo({
@@ -19,11 +21,24 @@ Page({
     }
   },
 
+  onShow() {
+    if (!app.globalData.msgContext || !app.globalData.msgContext.host) {
+      this.getMessageContext()
+    } else {
+      app.globalData.goEasy.subscribe({
+        channel: app.globalData.user.id, //替换为您自己的channel
+        onMessage: function (message) {
+          console.log(message)
+        }
+      })
+    }
+  },
+
   getDelivery: function () {
     var self = this
 
-    if (hasClick) return
-    hasClick = true
+    // if (hasClick) return
+    // hasClick = true
     wx.showLoading()
 
     wx.request({
@@ -36,7 +51,10 @@ Page({
           res.data.destination = res.data.destination.indexOf('@') === 0 ? res.data.destination.slice(1) : res.data.destination
           res.data.departure = res.data.departure ? res.data.departure.replace('@', ',') : ''
           res.data.destination = res.data.destination ? res.data.destination.replace('@', ',') : ''
-          self.setData({ delivery: res.data })
+          self.setData({ 
+            delivery: res.data,
+            target_user_id: res.data.travel.user.id
+          })
         } else {
           console.log(res)
           wx.showToast({ title: res.data.msg, icon: 'none' })
@@ -47,7 +65,7 @@ Page({
       },
       complete: function (res) {
         wx.hideLoading()
-        hasClick = false
+        // hasClick = false
       }
     })
   },
@@ -72,6 +90,13 @@ Page({
           self.setData({
             payment: res.data[0]
           })
+          if (self.data.payment.status === 'success') {
+            const params = {
+              type: 'text',
+              content: '我已经完成支付啦！'
+            }
+            self.addMessage(params)
+          }
         } else {
           console.log(res)
           wx.showToast({ title: res.data.msg, icon: 'none' })
@@ -83,6 +108,122 @@ Page({
       complete: function (res) {
         wx.hideLoading()
         hasClick = false
+      }
+    })
+  },
+
+  // 发布消息
+  addMessage(opts) {
+    var self = this
+    var params = opts
+    if (app.globalData.msgContext.host === '' || app.globalData.msgContext.host === null) {
+      return
+    }
+    if (params.type === '' || params.type === null) {
+      return
+    }
+    if (this.data.target_user_id === '' || this.data.target_user_id === null) {
+      wx.showToast({ title: '无效用户ID', icon: 'none' })
+      return
+    }
+    params.target_user_id = this.data.target_user_id
+
+    console.log(params)
+
+    // if (hasClick) return
+    // hasClick = true
+    wx.showLoading()
+
+    wx.request({
+      url: app.globalData.baseUrl + '/message/v1/messages',
+      method: 'POST',
+      header: {
+        'Authorization': 'Bearer ' + wx.getStorageSync('ashibro_Authorization'),
+        'IM-Host': app.globalData.msgContext.host,
+        'Content-Type': 'multipart/form-data; boundary=XXX'
+      },
+      data: formdata(params),
+      success: function (res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+        } else {
+          if (res.data.msg && res.data.msg.indexOf('Token Expired') !== -1) {
+            wx.navigateTo({
+              url: '/pages/user/auth/auth',
+            })
+          } else {
+            wx.showToast({ title: res.data.msg, icon: 'none' })
+          }
+        }
+      },
+      fail: function (res) {
+        wx.showToast({ title: '系统错误', icon: 'none' })
+      },
+      complete: function (res) {
+        wx.hideLoading()
+        // hasClick = false
+      }
+    })
+  },
+
+  // 消息服务上下文
+  getMessageContext() {
+    var self = this
+    // wx.showLoading()
+
+    wx.request({
+      url: app.globalData.baseUrl + '/message/v1/message-context',
+      method: 'GET',
+      success: function (res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          // 服务器回包内容
+          app.globalData.msgContext = res.data
+
+          // app.globalData.msgContext.host = 'https://rest-hangzhou.goeasy.io/publish'
+          // 在onLaunch方法里初始化全局GoEasy对象
+          app.globalData.goEasy = new GoEasy({
+            host: "hangzhou.goeasy.io", //应用所在的区域地址: 【hangzhou.goeasy.io | singapore.goeasy.io】
+            appkey: app.globalData.msgContext.app_key, //替换为您的应用appkey
+            onConnected: function () {
+              console.log('连接成功！')
+            },
+            onDisconnected: function () {
+              console.log('连接断开！')
+            },
+            onConnectFailed: function (error) {
+              console.log('连接失败或错误！')
+            }
+          })
+
+          if (app.globalData.user && app.globalData.user.id) {
+            app.globalData.goEasy.subscribe({
+              channel: app.globalData.user.id, //替换为您自己的channel
+              onMessage: function (message) {
+                console.log(message)
+                self.getMessage(message.content)
+              }
+            })
+          }
+
+        } else {
+          console.log(res)
+          if (res.errMsg && res.errMsg === 'request:ok') {
+            if (res.data.msg && res.data.msg.indexOf('Token Expired') !== -1) {
+              wx.navigateTo({
+                url: '/pages/user/auth/auth',
+              })
+            } else {
+              wx.showToast({ title: res.data.msg, icon: 'none' })
+            }
+          } else {
+            wx.showToast({ title: res.errMsg || '请求出错', icon: 'none' })
+          }
+        }
+      },
+      fail: function (res) {
+        wx.showToast({ title: '系统错误', icon: 'none' })
+      },
+      complete: function (res) {
+        // wx.hideLoading()
       }
     })
   }
