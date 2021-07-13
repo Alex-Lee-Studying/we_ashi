dccd3b14f736e841b81f2bcd2cbe7ee204fb6ae3
alex <liwenhuiaidan@sina.com>
2021-07-13 18:26:45 +0800

    流程页面

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -29,6 +29,35 @@ Page({
   onUnload: function() {
     clearInterval(interval)
   },
+  getBanners() {
+    var self = this
+
+    wx.request({
+      url: app.globalData.baseUrl + '/app/v1/banners',
+      method: 'GET',
+      header: { 'page': this.data.currPageTravel, 'page-size': this.data.pageSize, 'Authorization': 'Bearer ' + wx.getStorageSync('ashibro_Authorization') },
+      // data: params,
+      success: function (res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          if (res.data.length < self.data.pageSize) {
+            var reqState = false
+            self.setData({
+              noMoreTravelsFlag: true
+            })
+          } else {
+            var reqState = true
+          }
+          console.log(res.data)
+          
+          self.setData({
+            banner_list: res.data
+          })
+        } else {
+
+        }
+      },
+    })
+  },
   start() {
     wx.switchTab({
       url: '/pages/index/index'
